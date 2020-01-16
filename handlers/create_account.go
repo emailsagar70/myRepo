baf0c22e7c711f56994c78e8f125de266ef97501
emailsagar70 <57654701+emailsagar70@users.noreply.github.com>
2020-01-16 16:58:37 -0600

    new changes made

--- handlers/create_account.go
+++ handlers/create_account.go
@@ -11,14 +11,24 @@ var (
 )
 
 //CreateAccount is used to create account details
-func CreateAccount(w http.ResponseWriter, r *http.Request) {
+func CreateAccount(w http.ResponseWriter, req *http.Request) {
 	// this variable holds the decoded jason data
 	var account models.Account
 
-	body, err = ioutil.ReadAll(r.Body)
+	body, err := ioutil.ReadAll(req.Body)
+	// fmt.Println(body)
+	// fmt.Println(err)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest) // this will returnt the status code 400 when there is an error
 		// User will receive the data in byte form this will convert the normal error in a string by decoding
 		w.Write([]byte(err.Error()))
 	}
+}
+
+// It will decode all the data from json to code langugage
+err = json.Unmarshal (body, &account)
+if err != nil {
+	w.WriteHeader(http.StatusBadRequest) // this will returnt the status code 400 when there is an error
+	// User will receive the data in byte form this will convert the normal error in a string by decoding
+	w.Write([]byte(err.Error()))
 }

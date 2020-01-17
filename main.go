d80a908e6f145814b45dd3f2f59be250c5b76f7e
emailsagar70 <57654701+emailsagar70@users.noreply.github.com>
2020-01-17 01:26:33 -0600

    new changes

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import "github.com/tes-svc/handlers"
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("v1/account", handlers.CreateAccount).Methods(http.MethodPost)
+	r.HandleFunc("/v1/account", handlers.CreateAccount).Methods(http.MethodPost)
 
 	http.ListenAndServe(":8080", r)
 

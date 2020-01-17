dc347038969454e86db2b03342aeb8f9f901f45b
emailsagar70 <57654701+emailsagar70@users.noreply.github.com>
2020-01-17 00:36:32 -0600

    new changes

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-// import "github.com/gorilla/mux"
+import "github.com/gorilla/mux"
 
-// import "net/http"
+import "net/http"
+
+import "github.com/tes-svc/handlers"
 
 func main() {
-	//r:=mux.NewRouter()
+	r := mux.NewRouter()
+	r.HandleFunc("v1/account", handlers.CreateAccount).Methods(http.MethodPost)
+
+	http.ListenAndServe(":8080", r)
 
 }

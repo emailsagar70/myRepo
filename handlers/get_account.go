65a5904fcc864cbc09059610de947ca0be5f748f
emailsagar70 <57654701+emailsagar70@users.noreply.github.com>
2020-01-18 16:51:02 -0600

    new updates

---
+++ handlers/get_account.go
@@ -0,0 +1,21 @@
+package handlers
+
+import "net/http"
+
+import "encoding/json"
+
+// ListAccount is used to get the list of an account
+func ListAccount(w http.ResponseWriter, r *http.Request) {
+	// This will take the address of 'accounts' where all the 'Post'ed values are stored
+	JSONResp, err := json.Marshal(&accounts)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	// if it gets the value, it'll set status = 200
+	w.WriteHeader(http.StatusOK)
+	// after response 200 it will send the value in bytes form to frontend.
+	w.Write(JSONResp)
+}
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import "github.com/tes-svc/handlers"
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/v1/account", handlers.CreateAccount).Methods(http.MethodPost)
+	r.HandleFunc("/v1/account", handlers.ListAccounts).Methods(http.MethodGet)
 
 	http.ListenAndServe(":8080", r)
 
